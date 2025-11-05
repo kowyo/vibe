97d13c5715a4d1157bb6fe63bd78f28278fc1d69
Kowyo <kowyo@outlook.com>
2025-11-05 18:44:04 +0800

    set SidebarProvider to defaultOpen false

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans antialiased`}>
-        <SidebarProvider defaultOpen={true}>
+        <SidebarProvider defaultOpen={false}>
           <ProjectsSidebar />
           <SidebarInset>
             {children}
